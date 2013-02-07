dd5b85fa98406422591ac7ddea5bd1e3cc02626b
tbalden <illespal@gmail.com>
2013-02-07 23:07:21 +0100

    add extern value, to be able to unconfigure Hotplug options. (credit to Xmister)

--- arch/arm/mach-tegra/tegra3_throttle.c
+++ arch/arm/mach-tegra/tegra3_throttle.c
@@ -57,8 +57,8 @@ static unsigned int clip_to_table(unsigned int cpu_freq)
 	i = (i == 0) ? 0 : i-1;
 	return cpu_freq_table[i].frequency;
 }
-
-extern unsigned int no_thermal_throttle_limit;
+// no hotplug auto patch - by Xmister
+extern unsigned int no_thermal_throttle_limit=0;
 
 unsigned int tegra_throttle_governor_speed(unsigned int requested_speed)
 {
